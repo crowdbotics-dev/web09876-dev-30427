a7f54341e8178cfe12e0eb667867a7c650a36eb9
Crowdbotics <engineering@crowdbotics.com>
2022-08-26 05:12:04 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Nbhdjj
+
+admin.site.register(Nbhdjj)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Nbhdjj(models.Model):
+    "Generated Model"
+    uioo = models.BigIntegerField()
